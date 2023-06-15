9f5754ef0ae0aacc10fbdffc29db7aa0d8a8fcd5
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-06-15 09:26:32 +0430

    Empty store created and provided for the app

--- src/BookStore.jsx
+++ src/BookStore.jsx
@@ -1,7 +1,9 @@
 import '@/styles/Global.css'
-
+import { useSelector } from 'react-redux';
 
 function BookStore() {
+  const store = useSelector((store) => store);
+  console.log(store);
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from '@/redux/store.js'
+
 import BookStore from './BookStore.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BookStore />
+    <Provider store={store}>
+      <BookStore />
+    </Provider>
   </React.StrictMode>,
 )
