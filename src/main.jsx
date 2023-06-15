0b33ce92488358c5d4013b8bbdcfc03c761df809
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-06-15 23:59:20 +0430

    StrictMode temporarily removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from '@/redux/store.js'
@@ -6,9 +5,9 @@ import store from '@/redux/store.js'
 import BookStore from './BookStore.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <Provider store={store}>
       <BookStore />
     </Provider>
-  </React.StrictMode>,
+  </>,
 )
