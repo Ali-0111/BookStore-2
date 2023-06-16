cb3c993eb319d044557abc7ee728854c5efa6c19
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-06-16 23:00:58 +0430

    Header directory added

--- src/BookStore.jsx
+++ src/BookStore.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import Header from '@/components/Header.jsx';
+import Header from '@/components/Header/Header.jsx';
 import Routes from '@/components/Routes/RoutesConfig.jsx';
 import { getBooks_API } from '@/apiServices/apiFunc.js';
 import '@/styles/Global.css'
